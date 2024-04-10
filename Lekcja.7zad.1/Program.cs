e9128dd2941609b4e98b0f63ffe07c9952200524
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-04-10 19:35:43 +0200

    Add project files.

---
+++ Lekcja.7zad.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		int a = 5;
+		int b = 5;
+       
+		
+		if (a == b) 
+		{
+			Console.WriteLine($"{a} i {b} są równe.");	 
+
+		}
+		else
+		{
+			Console.WriteLine($"{a} i {b} nie są równe.");
+		}
+
+		Console.ReadLine();
+	}
+}
+	
